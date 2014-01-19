90570df0ea257116764c312a00f0711bc65a7385
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-01-19 15:11:38 -0500

    Fixed file so it's at the exact right spot for starting this branch

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ var app = express();
 var Datastore = require('nedb');
 var db = {};
 
+// Connect to an NeDB database
 db.movies = new Datastore({ filename: 'db/movies', autoload: true });
 
+// Necessary for accessing POST data via req.body object
 app.use(express.bodyParser());
 
+// Routes
 app.get('/', function (req, res) {
-
     res.send('The API is working.');
-
 })
 .post('/rpc', function (req, res) {
-
     var body = req.body
-    var movie;
-
     var respond = function (err, response) {
         if (err) {
             res.send(JSON.stringify(err));
@@ -28,23 +26,22 @@ app.get('/', function (req, res) {
     res.set('Content-type', 'application/json');
 
     switch (body.action) {  
-
         case "getMovies":
             db.movies.find({}, respond);
             break;
 
         case "addMovie":
-            req.body.title && db.movies.insert({ title: req.body.title }, respond);
+            db.movies.insert({ title: req.body.title }, respond);
             break;
 
         case "rateMovie":
             db.movies.update({ title: body.title }, { $set: { rating: body.rating } }, function (err, num) {
-                respond(err, { success: num + " record updated" });
+                respond(err, { success: num + " records updated" });
             });
             break;
 
         default:
-            respond({ error: "No action given in request." });
+            respond({ error: "No action given" });
     }
 
 })
