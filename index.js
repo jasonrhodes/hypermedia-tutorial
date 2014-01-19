ff0d586afa932cfdd92f4beb0edbce0533cb4193
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-01-19 13:33:55 -0500

    Finished upgrading to Level 1

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ var app = express();
 var Datastore = require('nedb');
 var db = {};
 
+var setupResponder = (function (res) {
+
+    return function (err, response) {
+        if (err) {
+            res.send(JSON.stringify(err));
+        } else {
+            res.send(JSON.stringify(response));
+        }
+    };
+
+});
+
 db.movies = new Datastore({ filename: 'db/movies', autoload: true });
 
 app.use(express.bodyParser());
@@ -12,18 +24,52 @@ app.get('/', function (req, res) {
     res.send('The API is working.');
 
 })
+.post('/movies', function (req, res) {
+    var body = req.body;
+    var respond = setupResponder(res);
+
+    res.set('Content-type', 'application/json');
+
+    switch (body.action) {
+
+        case "viewList":
+            db.movies.find({}, respond);
+            break;
+
+        case "addNew":
+            req.body.title && db.movies.insert({ title: req.body.title }, respond);
+            break;            
+
+        default:
+            respond({ error: "No action given in request." });
+    }
+})
+.post('/movies/:id', function (req, res) {
+    var body = req.body;
+    var respond = setupResponder(res);
+
+    switch (body.action) {
+
+        case "view":
+            db.movies.find({ _id: req.params.id }, respond);
+
+        case "rate":
+            db.movies.update({ _id: req.params.id }, { $set: { rating: body.rating } }, function (err, num) {
+                respond(err, { success: num + " record updated" });
+            });
+            break;
+
+        default:
+            respond({ error: "No action given in request." });
+
+    }
+
+})
 .post('/rpc', function (req, res) {
 
     var body = req.body
     var movie;
-
-    var respond = function (err, response) {
-        if (err) {
-            res.send(JSON.stringify(err));
-        } else {
-            res.send(JSON.stringify(response));
-        }
-    };
+    var respond = setupResponder(res);
     
     res.set('Content-type', 'application/json');
 
