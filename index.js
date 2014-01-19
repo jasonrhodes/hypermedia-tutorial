fa2068037f87a51774d0c0ca1caae2c92f14b4c5
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-01-19 14:47:07 -0500

    Prepared for the beginning of the 2-nouns branch (found in video 3-rmm1 I believe, this isn't confusing AT ALL)

--- index.js
+++ index.js
@@ -3,73 +3,27 @@ var app = express();
 var Datastore = require('nedb');
 var db = {};
 
-var setupResponder = (function (res) {
-
-    return function (err, response) {
-        if (err) {
-            res.send(JSON.stringify(err));
-        } else {
-            res.send(JSON.stringify(response));
-        }
-    };
-
-});
-
 db.movies = new Datastore({ filename: 'db/movies', autoload: true });
 
 app.use(express.bodyParser());
- 
+
 app.get('/', function (req, res) {
 
     res.send('The API is working.');
-
-})
-.post('/movies', function (req, res) {
-    var body = req.body;
-    var respond = setupResponder(res);
-
-    res.set('Content-type', 'application/json');
-
-    switch (body.action) {
-
-        case "viewList":
-            db.movies.find({}, respond);
-            break;
-
-        case "addNew":
-            req.body.title && db.movies.insert({ title: req.body.title }, respond);
-            break;            
-
-        default:
-            respond({ error: "No action given in request." });
-    }
-})
-.post('/movies/:id', function (req, res) {
-    var body = req.body;
-    var respond = setupResponder(res);
-
-    switch (body.action) {
-
-        case "view":
-            db.movies.find({ _id: req.params.id }, respond);
-
-        case "rate":
-            db.movies.update({ _id: req.params.id }, { $set: { rating: body.rating } }, function (err, num) {
-                respond(err, { success: num + " record updated" });
-            });
-            break;
-
-        default:
-            respond({ error: "No action given in request." });
-
-    }
 
 })
 .post('/rpc', function (req, res) {
 
     var body = req.body
     var movie;
-    var respond = setupResponder(res);
+
+    var respond = function (err, response) {
+        if (err) {
+            res.send(JSON.stringify(err));
+        } else {
+            res.send(JSON.stringify(response));
+        }
+    };
     
     res.set('Content-type', 'application/json');
 
@@ -94,4 +48,4 @@ app.get('/', function (req, res) {
     }
 
 })
-.listen(process.argv[2] || 3000);
+.listen(process.argv[2] || 3050);
