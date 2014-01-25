0e551dd33807cab78ed58e80bd2a52e320cd5750
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-01-25 14:46:20 -0500

    Finished recording HTTP Statuses video and made a few slight adjustments to the file so that it's ready to go for the next chapter

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ var Datastore = require('nedb');
 var db = {};
 var responder = require('./httpResponder');
 
+var port = process.argv[2] || 3050;
+var root = "http://localhost:" + port;
+
 // Connect to an NeDB database
 db.movies = new Datastore({ filename: 'db/movies', autoload: true });
+
+// Add an index
+db.movies.ensureIndex({ fieldName: 'title', unique: true });
 
 // Necessary for accessing POST data via req.body object
 app.use(express.bodyParser());
@@ -27,21 +33,72 @@ app.get('/movies', function (req, res) {
 });
 
 app.post('/movies', function (req, res) {
-    db.movies.insert({ title: req.body.title }, res.locals.respond);
+
+    if (!req.body.title) {
+        res.json(400, { error: { message: "A title is required to create a new movie." }});
+        return;
+    }
+
+    db.movies.insert({ title: req.body.title }, function (err, created) {
+        if (err) {
+            res.json(500, { error: err });
+            return;
+        }
+
+        res.set('Location', root + '/movies/' + created._id);
+        res.json(201, created);
+    });
 });
 
 app.get('/movies/:id', function (req, res) {
-    db.movies.findOne({ _id: req.params.id }, res.locals.respond);
+    db.movies.findOne({ _id: req.params.id }, function (err, result) {
+        if (err) {
+            res.json(500, { error: err });
+            return;
+        }
+
+        if (!result) {
+            res.json(404, { error: { message: "We did not find a movie with id: " + req.params.id }});
+            return;
+        }
+
+        res.json(200, result);
+    });
 });
 
 app.put('/movies/:id', function (req, res) {
-    db.movies.update({ _id: req.params.id }, req.body, function (err, num) {
-            res.locals.respond(err, { success: num + " records updated" });
-        });
+    db.movies.update({ _id: req.params.id }, req.body, { upsert: false }, function (err, num, upsert) {
+        
+        if (err) {
+            res.json(500, { error: err });
+            return;
+        }
+
+        if (num === 0) {
+            res.json(400, { error: { message: "No records were updated." }});
+            return;
+        }
+
+        res.send(204);
+        res.json(200, { success: { message: "Sucessfully updated movie with ID " + req.params.id }});
+    });
 });
 
 app.delete('/movies/:id', function (req, res) {
-    db.movies.remove({ _id: req.params.id }, res.locals.respond);
+    db.movies.remove({ _id: req.params.id }, function (err, num) {
+        if (err) {
+            res.json(500, { error: err });
+            return;
+        }
+
+        if (num === 0) {
+            res.json(404, { error: { message: "We did not find a movie with id: " + req.params.id }});
+            return;
+        }
+
+        res.set('Link', root + '/movies; rel="collection"');
+        res.send(204);
+    });
 });
 
-app.listen(process.argv[2] || 3050);
+app.listen(port);
