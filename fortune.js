312f1630f0207b45ea256fa87abe969c5eb0e0d0
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-01-31 00:42:35 -0500

    Work done in last video complete

--- fortune.js
+++ fortune.js
@@ -1,1 +1,25 @@
 var fortune = require("fortune");
+
+var app = fortune({
+	db: "./db/hypermovies",
+	baseUrl: "http://localhost:4000"
+});
+
+app.resource('movie', {
+	title: String,
+	rating: Number,
+	director: 'director',
+	cast: ['actor']
+});
+
+app.resource('director', {
+	name: String,
+	movies: ['movie']
+});
+
+app.resource('actor', {
+	name: String,
+	movies: ['movie']
+});
+
+app.listen(4000);
